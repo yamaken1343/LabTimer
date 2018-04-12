39f988cd4663b56bce1de1f14f3f251a31eebe36
yamaken <yamaken1343@gmail.com>
2018-04-12 12:56:22 +0900

    first

---
+++ LabT.java
@@ -0,0 +1,23 @@
+class LabT {
+    private long time = 0;
+    private boolean status = false;
+
+    void timeCount(){
+        if (status) time++;
+    }
+    void reset(){
+        time = 0;
+    }
+    long getTime(){
+        return time;
+    }
+    void active(){
+        status = true;
+    }
+    void inactive(){
+        status = false;
+    }
+    String formatPrint(){
+        return String.format("%02d:%02d:%02d", time/3600, time/60, time%60);
+    }
+}
