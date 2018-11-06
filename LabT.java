dfb14301af6fa128b572a7fff63aa67882de683e
yamaken <yamaken1343@gmail.com>
2018-11-06 15:54:26 +0900

    時間を加算ではなく開始時刻からの差で取得するように

--- LabT.java
+++ LabT.java
@@ -1,26 +1,40 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 class LabT {
     private long time = 0;
+    private long elapsedTime = 0;
     private boolean status = false;
+    private LocalDateTime startTime;
+
+    LabT(){
+        startTime = LocalDateTime.now();
+    }
 
     void timeCount(){
-        if (status) time++;
+        if (status) time = ChronoUnit.SECONDS.between(startTime, LocalDateTime.now());
     }
     void reset(){
-        time = 0;
+        elapsedTime = 0;
+        startTime = LocalDateTime.now();
     }
     long getTime(){
-        return time;
+        return elapsedTime;
     }
     boolean getStatus() {
         return status;
     }
     void active(){
+        inactive();
         status = true;
+        startTime = LocalDateTime.now();
     }
     void inactive(){
         status = false;
+        elapsedTime += time;
+        time = 0;
     }
     String formatPrint(){
-        return String.format("%02d:%02d:%02d", time/3600%60, time/60%60, time%60);
+        return String.format("%02d:%02d:%02d", (elapsedTime+time)/3600%60, (elapsedTime+time)/60%60, (elapsedTime+time)%60);
     }
 }
